27607131fd9bfce35bcfc3b7eb3192aa5d1e5eca
Chingsan0722 <rory85722@gmail.com>
2023-07-15 14:29:59 +0800

    add errorHandler to handle API error

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 const express = require('express')
+const router = require('./routes')
 const cors = require('cors')
 const path = require('path')
 const passport = require('passport')
-// const router = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 // const corsOptions = {
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(passport.initialize())
 app.use('/upload', express.static(path.join(__dirname, 'upload')))
-// app.use('/api', router)
+app.use('/api', router)
 app.get('/', (req, res) => res.send('Titan Shop API Server.'))
 app.listen(port, () => console.info(`Example app listening on port ${port}!`))
 module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const { apiErrorHandler } = require('../helpers/errorHandler')
+const userController = require('../controllers/userController')
 // const passport = require('../config/passport')
 
+router.get('/user/:id', userController.getUser)
 router.get('/', (req, res) => {
   res.send('hello')
 })
+router.use('/', apiErrorHandler)
+
+module.exports = router

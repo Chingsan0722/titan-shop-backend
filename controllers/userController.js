c6aabbea049824fb4fcb022c06b8781b215d6b9b
Chingsan0722 <rory85722@gmail.com>
2023-07-15 15:43:32 +0800

    add signIn api by use JWT & Local Auth

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const { User } = require('../models')
+const jwt = require('jsonwebtoken')
 const userController = {
   getUser: async (req, res, next) => {
     try {
@@ -6,6 +7,18 @@ const userController = {
       const data = await User.findAll({ where: { id: userId } })
       if (data.length === 0) return res.status(404).json({ message: 'User not found' })
       res.json(data)
+    } catch (error) {
+      next(error)
+    }
+  },
+  signIn: async (req, res, next) => {
+    try {
+      const userData = req.user.toJSON()
+      delete userData.password
+      const token = jwt.sign(userData, process.env.JWT_SECRET, {
+        expiresIn: '7d'
+      })
+      return res.status(200).json({ token, userData })
     } catch (error) {
       next(error)
     }
--- seeders/20230715033036-demo-user.js
+++ seeders/20230715033036-demo-user.js
@@ -5,14 +5,14 @@ module.exports = {
     await queryInterface.bulkInsert('Users', [{
       name: 'buyer001',
       account: 'buyer001',
-      password: await bcrypt.hash('12345678', 10),
+      password: await bcrypt.hash('titaner', 10),
       role: 'user',
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       name: 'seller001',
       account: 'seller001',
-      password: await bcrypt.hash('', 10),
+      password: await bcrypt.hash('titaner', 10),
       role: 'admin',
       createdAt: new Date(),
       updatedAt: new Date()

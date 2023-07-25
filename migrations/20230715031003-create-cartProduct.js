7df3196e478375b2080efe74a618362897f1e61e
Chingsan0722 <rory85722@gmail.com>
2023-07-25 15:17:11 +0800

    fix aws error by upperCase

--- migrations/20230715031003-create-cartProduct.js
+++ migrations/20230715031003-create-cartProduct.js
@@ -39,6 +39,6 @@ module.exports = {
     })
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('cartProducts')
+    return queryInterface.dropTable('CartProducts')
   }
 }

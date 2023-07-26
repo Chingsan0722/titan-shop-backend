18a2a5ee5fbe1de8bf9aad50d58eb206db7516c5
Chingsan0722 <rory85722@gmail.com>
2023-07-26 17:26:17 +0800

    add user seed to 5

--- seeders/20230715033036-demo-user.js
+++ seeders/20230715033036-demo-user.js
@@ -3,13 +3,6 @@ const bcrypt = require('bcryptjs')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Users', [{
-      name: 'buyer001',
-      account: 'buyer001',
-      password: await bcrypt.hash('titaner', 10),
-      role: 'user',
-      created_at: new Date(),
-      updated_at: new Date()
-    }, {
       name: 'seller001',
       account: 'seller001',
       password: await bcrypt.hash('titaner', 10),
@@ -17,8 +10,20 @@ module.exports = {
       created_at: new Date(),
       updated_at: new Date()
     }])
+    const usersData = []
+    for (let i = 1; i < 6; i++) {
+      const userData = {
+        name: `buyer00${i}`,
+        account: `buyer00${i}`,
+        password: await bcrypt.hash('titaner', 10),
+        role: 'user',
+        created_at: new Date(),
+        updated_at: new Date()
+      }
+      usersData.push(userData)
+    }
+    await queryInterface.bulkInsert('Users', usersData)
   },
-
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete('Users', null, {})
   }
